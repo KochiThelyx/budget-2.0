65ec2ce9dba54f7274b8a7786a542c12bec43209
KochiThelyx <philippkoch0@gmail.com>
2025-06-10 11:36:52 +0200

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'knetenking-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/vertraege.html',
+  '/to-do.html',
+  '/Einkaufsliste.html',
+  '/settings.html',
+  '/script.js',
+  '/sw-register.js',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
